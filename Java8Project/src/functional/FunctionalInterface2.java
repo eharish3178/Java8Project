0a584449532e307c01218e90c6eb9c967e0b16ed
eharish3178 <eharish3178@gmail.com>
2022-07-06 19:39:30 +0530

    commit

---
+++ Java8Project/src/functional/FunctionalInterface2.java
@@ -0,0 +1,11 @@
+package functional;
+
+@FunctionalInterface
+interface FunctionalInterface2 extends FunctionalInterface1 {
+
+    void method1();
+
+    static void staticM1() {
+	System.out.println("FunctionalInterface2-staticM1");
+    }
+}
